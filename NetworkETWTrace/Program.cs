46373fb92a2d7b719ffd29789819e829a19cc173
Leonard Gläser <dev@leonardglaeser.de>
2023-03-21 23:13:40 +0100

    first poc

---
+++ NetworkETWTrace/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Session;
+
+
+using (var NetEventSession = new TraceEventSession("NetEventSession")){
+    NetEventSession.EnableProvider("Microsoft-Windows-TCPIP");
+    using (var source = new ETWTraceEventSource("NetEventSession",TraceEventSourceType.Session))
+    {
+        // Add Callback to Parser?
+        source.Dynamic.All += (TraceEvent data) => {
+            Console.WriteLine($"{data.EventName} : {data.FormattedMessage}");
+        };
+        source.Process(); // Invoke the callbacks
+    }
+    //Todo:  not closing the session as never gracefully stopping
+    //Not closing the
+}
